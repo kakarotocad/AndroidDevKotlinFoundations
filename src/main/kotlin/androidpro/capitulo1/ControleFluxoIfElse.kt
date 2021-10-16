0bb61d5785a93ee6674805ac24949e47574480d3
Sergio Pereira <devkakaroto@Sergios-MacBook-Pro.local>
2021-10-16 08:30:59 -0600

    Initial commit

---
+++ src/main/kotlin/androidpro/capitulo1/ControleFluxoIfElse.kt
@@ -0,0 +1,11 @@
+package androidpro.capitulo1
+
+fun main() {
+    var idade = 36
+
+    if(idade<18){
+        println("Nao pode beber nem tirar carteira")
+    }else{
+        println("Ta tudo liberado")
+    }
+}
